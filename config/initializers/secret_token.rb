3919d59a9a766e67153213f984ee56496ba7697a
Ross Kinney <railspup@gmail.com>
2013-11-07 21:54:37 -0700

    Initial commit of generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikisite::Application.config.secret_key_base = 'ce025938e5ebd73b08e904cb5580d9c36d0149dbc72a039445679326a4197153e1940fd308a16a77addf9a19d6c9948fd530a3796692226217f12e69c365c2e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wikisite::Application.config.session_store :cookie_store, key: '_wikisite_session'
